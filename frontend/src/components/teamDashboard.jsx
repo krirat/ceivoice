0bdfa9add98c58ccb65034b9c47488bfc47102b1
RattK <37528531+krirat@users.noreply.github.com>
2026-02-17 14:36:40 +0700

    use ticketInfo modal

--- frontend/src/components/teamDashboard.jsx
+++ frontend/src/components/teamDashboard.jsx
@@ -1,40 +1,77 @@
 import React, { useState } from 'react';
+import Modal from 'react-modal';
+import TicketInfo from './ticketInfo';  
 import { FaTrash, FaCheck, FaSignOutAlt, FaPlus, FaClipboardList, FaCoffee, FaCalendarAlt} from 'react-icons/fa';
 
 const CEI_LOGO_URL = "https://cei.kmitl.ac.th/wp-content/uploads/2024/09/cropped-ceip-fav-1.png"; 
 const API_URL = import.meta.env.VITE_API_URL;
 
-const dataList = [
-    { id: 1, title: 'First Item' },
-    { id: 2, title: 'Second Item' },
-    { id: 3, title: 'Third Item' },
+const tickets = [
+    { id: 1, title: 'First Item', status: 'Open', assignee: 'Jane Doe', dueDate: '2028-01-02' },
+    { id: 2, title: 'Second Item', status: 'In Progress', assignee: 'John Smith', dueDate: '2028-01-03' },
+    { id: 3, title: 'Third Item', status: 'Closed', assignee: 'Alice Johnson', dueDate: '2028-01-01' },
+];
+
+const performanceMetrics = [
+    { id: 1, label: 'Tickets', value: 100 },
+    { id: 2, label: 'Solved', value: 10 },
+    { id: 3, label: 'Failed', value: 5 },
 ];
 
 function CustomerServiceDashboard() {
 
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
     return (
-        <div className="flex flex-col w-screen p-4">
-            <div className="flex text-center justify-around flex-row">
-                <div>
-                    <h1>30</h1>
-                    <h2>Tickets</h2>
-                </div>
-                <div>
-                    <h1>30</h1>
-                    <h2>Solved</h2>
-                </div>
-                <div>
-                    <h1>30</h1>
-                    <h2>Failed</h2>
-                </div>
+        <div className="flex flex-col w-screen p-4 text-gray-700 dark:text-gray-300 bg-gray-50 dark:bg-gray-800">
+            <Modal isOpen={modalIsOpen} className="rounded-xl bg-white dark:bg-gray-700 p-4 w-1/2 min-w-[300px] mx-auto mt-20 border-2 border-gray-300">
+                <TicketInfo closeTicket={() => setModalIsOpen(false)} />
+            </Modal>
+            <div className="my-20 flex text-center justify-around flex-row">
+                {performanceMetrics.map(metric => (
+                    <div key={metric.id}>
+                        <h1 className="text-5xl font-bold my-4">{metric.value}</h1>
+                        <h2 className='text-2xl'>{metric.label}</h2>
+                    </div>   
+                ))}
             </div>
-            <div>
-                <ul>
-                    {dataList.map(item => (
-                        <li key={item.id}>{item.title}</li>
+            <h1 className="text-3xl font-bold my-4">Tickets:</h1>
+            <div className="flex justify-center max-w-[180px] p-2 rounded-full bg-white dark:bg-gray-700">
+            //TODO: Add filter functionality
+            <select name="filter" >
+                <option className="flex justify-center space-x-4 mt-4">Assigned Tickets</option>
+                <option className="flex justify-center space-x-4 mt-4">Unassigned Tickets</option>
+                <option className="flex justify-center space-x-4 mt-4">All Tickets</option>
+
+            </select>
+            </div>
+            
+            {/* <ul className="rounded-lg border border-gray-300 dark:border-gray-600 p-4 mt-4 bg-white dark:bg-gray-700 divide-y divide-gray-300 dark:divide-gray-600">
+                {dataList.map(item => (
+                    <li key={item.id} className='p-2'><button onClick={() => setModalIsOpen(true)}>{item.title}</button></li>
+                ))}
+            </ul> */}
+            <table className="w-full mt-10 ">
+                <thead>
+                    <tr>
+                    <th>Ticket</th>
+                    <th>Status</th>
+                    <th>Assignee</th>
+                    <th>Due Date</th>
+                    </tr>
+                </thead>
+               
+                <tbody>
+                    {tickets.map(ticket => (
+                        <tr key={ticket.id}>
+                            <td className="border p-2">{ticket.title}</td>
+                            <td className="border p-2">{ticket.status}</td>
+                            <td className="border p-2">{ticket.assignee}</td>
+                            <td className="border p-2">{ticket.dueDate}</td>
+                        </tr>
                     ))}
-                </ul>
-            </div>
+                </tbody>
+            </table>
         </div>
     )
 }
