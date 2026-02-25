817a04384029a850fb65f66b4c9ceae7d7578c30
RattK <37528531+krirat@users.noreply.github.com>
2026-02-25 19:03:31 +0700

    added protected routes

--- frontend/src/route/AppRouter.jsx
+++ frontend/src/route/AppRouter.jsx
@@ -6,22 +6,26 @@ import Adminassignee from "@/components/Admin/AdminAssignee";
 import Login from "../components/Login";
 import Signup from "../components/Signup";
 import CustomerServiceDashboard from "../pages/teamDashboard";
-import { CardDemo } from "../pages/TicketSubmit"
+import { CardDemo } from "../pages/TicketSubmit";
+import ProtectedRoute from "../hooks/privateRoutes";
 
-const AppRouter = () =>{
-    return(
-    <Routes>
-        <Route path="/" element={<Navigate to="/admin" />} />
-        <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<AdminDashboard />} />
-            <Route path="tickets" element={<AdminTicket />} />
-            <Route path="assignee" element={<Adminassignee/>}/>
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/cs-dashboard" element={<CustomerServiceDashboard />} />
-        <Route path="/ticket-submit" element={<CardDemo />} />
-    </Routes>
+const AppRouter = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate to="/admin" />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/ticket-submit" element={<CardDemo />} />
+            <Route element={<ProtectedRoute />}>
+
+                <Route path="/admin" element={<AdminLayout />}>
+                    <Route index element={<AdminDashboard />} />
+                    <Route path="tickets" element={<AdminTicket />} />
+                    <Route path="assignee" element={<Adminassignee />} />
+                </Route>
+                <Route path="/cs-dashboard" element={<CustomerServiceDashboard />} />
+            </Route>
+        </Routes>
     );
 };
 export default AppRouter;
