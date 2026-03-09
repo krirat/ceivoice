4ddff0224c333d1350f811881d1510b046ce1076
RattK <37528531+krirat@users.noreply.github.com>
2026-03-09 18:54:36 +0700

    fixed statuses

--- frontend/src/components/ticketInfo.jsx
+++ frontend/src/components/ticketInfo.jsx
@@ -6,7 +6,7 @@ import EventLogSection from "./eventLogSection";
 const API_URL = import.meta.env.VITE_API_URL;
 
 
-const statuses = ["Draft", "Open", "In Progress", "Resolved", "Closed"];
+const statuses = ["Draft", "New", "Assigned", "Solving", "Resolved", "Failed"];
 
 function TicketInfo({ closeTicket, ticketId }) {
     const [loading, setLoading] = useState(true);
